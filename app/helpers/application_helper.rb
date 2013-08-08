fbd606c421e216c5b355831df07f6d0dbf4ff2a5
Scott Fey <scottfey@gmail.com>
2013-08-08 13:44:53 -0500

    Finish static pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  
+  # these methods are available in the views
+  def full_title(page_title)
+    base_title = "Bespoke Tailor Application"
+    if page_title.empty?
+      base_title
+    else
+      "#{base_title} | #{page_title}"  
+    end
+  end  
 end
---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,55 @@
+require 'spec_helper'
+
+describe "StaticPages" do
+  
+  let(:base_title) { "Bespoke Tailor Application"}
+  
+  describe "Home page" do
+    it "should have the content 'Title'" do
+      visit '/static_pages/home'
+      expect(page).to have_content('Home')
+    end
+    
+    it "should have the correct title" do
+      visit '/static_pages/home'
+      expect(page).to have_title("#{base_title} | Home")
+    end
+  end
+  
+  describe "About page" do
+    it "should have the content 'About Us'" do
+      visit '/static_pages/about'
+      expect(page).to have_content('About Us')
+    end
+    
+    it "should have the correct title" do
+      visit '/static_pages/about'
+      expect(page).to have_title("#{base_title} | About Us")
+    end
+  end  
+  
+  describe "Help page" do
+    it "should have the content 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_content('Help')
+    end
+    
+    it "should have the correct title" do
+      visit '/static_pages/help'
+      expect(page).to have_title("#{base_title} | Help")
+    end
+  end  
+  
+  describe "Contact page" do
+    
+    it "should have the content 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact')
+    end
+  
+    it "should have the correct title" do
+      visit '/static_pages/contact'
+      expect(page).to have_title("#{base_title} | Contact Us")
+    end
+  end  
+end
